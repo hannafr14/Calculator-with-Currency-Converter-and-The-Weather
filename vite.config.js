d35ac57c02908ae92ec41bf365df2d464821cb4b
Hanna <hannafrolova14@gmail.com>
2026-07-20 13:58:30 +0200

    chore: configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Calculator-with-Currency-Converter-and-The-Weather/',
   plugins: [
     vue(),
     vueDevTools(),
